ce3fe42f720938a5baa38b5f87fd8b2130eef904
Matthew Mistele <mmistele@stanford.edu>
2016-11-27 00:06:20 -0500

    Adding a few things while showing Eddy around Unity

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Wink leftEye;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (leftEye.eyeOpen);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Wink.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wink : MonoBehaviour {
+
+	public Sprite openSprite;
+	public Sprite closedSprite;
+	public bool eyeOpen = false;
+	public const int LEFT_MOUSE_BUTTON = 0;
+
+	// Use this for initialization
+	void Start () {
+		GameManager GM = GameObject.FindGameObjectWithTag ("GameController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (LEFT_MOUSE_BUTTON)) {
+			Sprite nextSprite = openSprite;
+			if (eyeOpen) {
+				nextSprite = closedSprite;
+			}
+			gameObject.GetComponent<SpriteRenderer> ().sprite = nextSprite;
+			eyeOpen = !eyeOpen;
+		}
+	}
+}
